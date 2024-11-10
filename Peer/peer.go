01366df459de5ab08bed5e5afd5106b6031fa5bf
fbor <fbor@itu.dk>
2024-11-10 19:57:54 +0100

    critical

--- Peer/peer.go
+++ Peer/peer.go
@@ -1,10 +1,12 @@
 package peer
 
 import (
+	"fmt"
 	critical "github.com/DiSysCBFA/Handind-4/Critical-section"
 	"github.com/DiSysCBFA/Handind-4/h4"
 	"google.golang.org/grpc"
 	_ "google.golang.org/grpc/status"
+	"log"
 	"time"
 )
 
@@ -29,8 +31,14 @@ func NewPeer(id int, port int) *Peer {
 
 func (p *Peer) multicast() {
 	req := h4.RequestMessage{Id: int64(p.id), Timestamp: time.Now().UnixNano()}
-	p.requests <- req
-}
+	for _, port := range ports {
+		conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", peer.port), grpc.WithInsecure())
+		if err != nil {
+			log.Println(err)
+			continue
+
+	}
+
 
 func (p *Peer) access() {
 	if p.status == h4.Status_GRANTED {
