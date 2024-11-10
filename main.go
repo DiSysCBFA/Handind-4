60bbf265b49af07195456b280926be83b0075529
cslj <cslj@itu.dk>
2024-11-10 17:21:57 +0100

    file reader

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("ports.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	r := bufio.NewReader(file)
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		log.Println(line)
+	}
+
+	defer file.Close()
+}
