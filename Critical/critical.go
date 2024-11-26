4f52d86cfe0ba0bb4b1ebb8cd8d556630c911242
cslj <cslj@itu.dk>
2024-11-26 12:51:24 +0100

    prompt

--- Critical/critical.go
+++ Critical/critical.go
@@ -1,7 +1,31 @@
 package critical
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
 
 func Main() {
 	fmt.Println("Critical section has been accessed")
+
+	selectionQuit := promptui.Select{
+		Label: "Do you want to quit?",
+		Items: []string{"Yes", "No"},
+	}
+
+	_, result, err := selectionQuit.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed: %v\n", err)
+	}
+
+	// Check the user's selection
+	if result == "Yes" {
+		log.Println("Exiting the program")
+		os.Exit(0)
+	} else {
+		fmt.Println("Continuing the program")
+	}
 }
